9701d290f38ec5598046675a4e40c24b7814bd66
elargo <elargor@gmail.com>
2025-07-23 21:49:54 -0500

    Configuracion de BDD

---
+++ enrollment-ms/src/main/java/com/icodeap/enrollment/EnrollmentMsApplication.java
@@ -0,0 +1,13 @@
+package com.icodeap.enrollment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnrollmentMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnrollmentMsApplication.class, args);
+	}
+
+}
