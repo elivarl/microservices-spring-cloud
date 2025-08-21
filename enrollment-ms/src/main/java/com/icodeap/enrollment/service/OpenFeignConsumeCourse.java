bdddf73f832acae49bf257c728ed93b3bdcc299f
elargo <elargor@gmail.com>
2025-08-21 17:43:41 -0500

    Registro, descubrimiento de microservicios y balanceo de carga con Spring Cloud Eureka Server, Open Feing y Spring Cloud Balancer

--- enrollment-ms/src/main/java/com/icodeap/enrollment/service/OpenFeignConsumeCourse.java
+++ enrollment-ms/src/main/java/com/icodeap/enrollment/service/OpenFeignConsumeCourse.java
@@ -6,7 +6,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url = "http://localhost:8082", value = "course-ms")
+//@FeignClient(url = "http://localhost:8082", value = "course-ms")
+@FeignClient(name = "course-ms")
 public interface OpenFeignConsumeCourse {
     @GetMapping("/api/v1/courses/course-code/{code}")
     public CourseDTO findByCourseCode(@PathVariable("code") String courseCode);
--- enrollment-ms/src/main/java/com/icodeap/enrollment/service/OpenFeignConsumeStudent.java
+++ enrollment-ms/src/main/java/com/icodeap/enrollment/service/OpenFeignConsumeStudent.java
@@ -5,7 +5,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient (url = "http://localhost:8081", value = "students-ms")
+//@FeignClient (url = "http://localhost:8081", value = "students-ms")
+@FeignClient (name = "students-ms")
 public interface OpenFeignConsumeStudent {
     @GetMapping("/api/v1/students/identification-number/{identification}")
     public StudentDTO findByIdentificationNumber(@PathVariable("identification") String identificationNumber);
