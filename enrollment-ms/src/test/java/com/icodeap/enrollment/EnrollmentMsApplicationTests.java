b336486edef63f99f595c84e73e77f78bfe5b363
elargo <elargor@gmail.com>
2025-08-26 22:13:12 -0500

    Implementacion de Spring Cloud API GATEWAY

---
+++ enrollment-ms/src/test/java/com/icodeap/enrollment/EnrollmentMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icodeap.enrollment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnrollmentMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ students-ms/src/test/java/com/icodeap/students/StudentsMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icodeap.students;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
